ece0d3befa96493dea149a9bdb573a298b4e26a8
I438161 <p.vanhooren@student.fontys.nl>
2022-06-17 10:59:13 +0200

    :construction_worker: :white_check_mark: Implemented K6 for performance testing and added a stage for this to the pipeline

---
+++ k6/script.js
@@ -0,0 +1,25 @@
+import http from 'k6/http';
+import { check, sleep } from 'k6';
+
+export const options = {
+    stages: [
+        { duration: '2m', target: 100 },
+        { duration: '5m', target: 300 },
+        { duration: '30s', target: 500 },
+        { duration: '2m', target: 200 },
+        { duration: '1m', target: 0 }
+    ],
+};
+
+export default function () {
+    const res = http.get('http://vybes-profile-microservice/all');
+    const res2 = http.get('http://vybes-profile-microservice/search/dn/Pim');
+    const res3 = http.get('http://vybes-krabbel-microservice/profile');
+    const res4 = http.get('http://vybes-krabbel-microservice/profile/pid/with/23');
+    
+    check(res, { 'status was 200': (r) => r.status == 200 });
+    check(res2, { 'status was 200': (r) => r.status == 200 });
+    check(res3, { 'status was 200': (r) => r.status == 200 });
+    check(res4, { 'status was 200': (r) => r.status == 200 });
+    sleep(1);
+}
